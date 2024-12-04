0ca0e51e428027b126a35cb67d1c61a6a5171cb9
Aditya Raj <raj.adi792@gmail.com>
2024-12-04 23:53:18 +0530

    .

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,8 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        // target: 'http://localhost:5000', // Backend server URL
-        target: 'https://ipl-server-dxtv.onrender.com',
+         target: 'http://localhost:5000', // Backend server URL
+        // target: 'https://ipl-server-dxtv.onrender.com',
         changeOrigin: true, // Updates the `Origin` of the request to match the target
       },
     },
